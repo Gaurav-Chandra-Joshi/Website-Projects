a0442740a424d11eae263ab941253dfb30633371
Gaurav Chandra Joshi <92198241+classwer@users.noreply.github.com>
2025-07-28 16:44:17 +0530

    Update script.js
    This is the new script file for  the new HTML code.

--- Source Fetcher/script.js
+++ Source Fetcher/script.js
@@ -2,37 +2,81 @@ let inputBox = document.getElementById('input-box');
 let outputBox = document.getElementById('output-box');
 let button = document.getElementById('button');
 let outputCopyButton = document.getElementById("output-copy-button");
+let whatToSelect = document.getElementById('what-to-select');
+let metadata = document.getElementById("meta-data");
 
-function getSrc(input) {
-    inputBox.value = input;
+let properties = {};
+function optionFetcher() {
     let text = inputBox.value.trim();
     if (text.split(" ")[0] === `<iframe`) {
         console.log(`The getSrc() function is working properly!`)
-        let output = text.split(`src`)[1].split(" ")[0].split(`"`)[1];
+        let output = text.split(` `);
+
+        for (let i = 1; i < output.length; i++) {
+            let option = document.createElement("option");
+
+            if (output[i].includes(`=`)) {
+                // Putting the value in an object
+                properties[output[i].split(`=`)[0]] = output[i].split(`=`)[1].split(`"`)[1];
+
+                option.innerText = output[i].split(`=`)[0];
+                whatToSelect.appendChild(option);
+
+            }
+
+        }
+        console.log(properties)
         console.log(output);
-        outputBox.value = output;
     } else {
-        outputBox.value = "";
+        inputBox.value = "";
+        outputBox.value = "You have not given appropriate iframe!";
         alert(`Please give the appropriate data!`);
     }
 }
+function getSrc(input) {
+    inputBox.value = input;
+}
+
+function getTheOutput(propertyName) {
+    outputBox.value = properties[`${propertyName}`];
+}
+
+inputBox.addEventListener('input', () => {
+    optionFetcher();
+})
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
     if (inputBox.value === " " || inputBox.value === "") {
-        alert(`Please enter the iframe data first!`)
+        alert(`Please enter the iframe data first!`);
     } else {
         getSrc(inputBox.value);
     }
+    getTheOutput(whatToSelect.value);
+
+    showMetadata();
 })
 
 outputCopyButton.addEventListener('click', (e) => {
     e.preventDefault();
-    // navigator.clipboard.writeText(`The phrase "been there, done that" is an idiom used to express that someone has already experienced something that is being discussed or suggested. It can imply a sense of familiarity, boredom, or even dismissal, suggesting that the speaker has no desire to repeat the experience!`);
-    if(outputBox.value.trim() == ""){
+    if (outputBox.value.trim() == "") {
         alert(`First you will have to click on Get URL button!`);
-    }else{
+    } else {
         navigator.clipboard.writeText(outputBox.value);
     }
 })
-navigator.doNotTrack;
+
+function showMetadata() {
+    let tagName = document.getElementById("tag-name");
+    let attributeWithValue = document.getElementById("attribute-with-value");
+
+    let text = inputBox.value.trim();
+    let output = text.split(` `);
+    let i = 0;
+
+    tagName.innerText = `This the the <${text.split(" ")[0].split("<")[1]}> tag.`;
+    console.log(properties.length);
+
+    attributeWithValue.innerText = `This tag has "${text.split(`="`).length - 1}" attributes with value.`
+    console.log(tagName);
+}
