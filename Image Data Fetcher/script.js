d067e928cb4973c627076b5e827356d30b00691f
Gaurav Chandra Joshi <92198241+classwer@users.noreply.github.com>
2025-09-01 19:06:53 +0530

    Update script.js
    This code will now remove all the files submitted by the user. So that the user will not face the data repeating problem. It also remove the instruction row after the data input.

--- Image Data Fetcher/script.js
+++ Image Data Fetcher/script.js
@@ -1,43 +1,33 @@
 const inputForm = document.getElementById('input-form');
 const inputFile = document.getElementById("input-file");
 let table = document.getElementById("data-table");
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 inputForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    // alert(`Thanks for your submission "${prompt("Tell me you name?")}"!`);
+    table.deleteRow(1);
 
     // Fetching the information related to the inputted file
     fetchInputInformation();
+    inputFile.value = "";
+    alert("All files data has been loaded!");
 })
 
 function fetchInputInformation() {
     const submittedImage = inputFile.files;
-    console.log(submittedImage);
 
     let demoData = `File Name :- "${submittedImage[0].name}"`
-    submittedImage.length < 1 ? alert("Please first submit file!") : console.log(demoData);
+    submittedImage.length < 1 ? alert("Please first submit file!") : null;
+    // submittedImage.length < 1 ? alert("Please first submit file!") : console.log(demoData);
 
     addTableData(submittedImage);
 
-    // submittedImage.length > 1 ? fetchMultipleFilesData() : fetchSingleFileData();
-
-    // Here I have to work on the data fetching . When the submit button will be clicked then the data related to that will be shown in and table.
-
-    // function fetchMultipleFilesData(){
-    //     console.log(`Fetching multiple file data ...`);
-    // }
-    // function fetchSingleFileData(){
-    //     console.log(`Fetching single file data ...`);
-    // }
 }
 
 function addTableData(fileListData) {
 
     console.log(fileListData);
 
-    
-    
-    
     for (let i = 0; i < fileListData.length; i++) {
         let tableData = document.createElement('tr');
 
@@ -45,28 +35,28 @@ function addTableData(fileListData) {
         let name = document.createElement('td');
         let size = document.createElement('td');
         let type = document.createElement('td');
-        
-        sNo.textContent = (table.children[0].children.length ) + ".";
-        function demoFnx(){
+        let lastModifiedDate = document.createElement('td');
+        let lastModifiedDay = document.createElement('td');
+
+        sNo.textContent = (table.children[0].children.length) + ".";
+        function demoFnx() {
             name.textContent = fileListData[i].name;
-            size.textContent = fileListData[i].size;
-            type.textContent = fileListData[i].name.split(".")[1];
+            size.textContent = Math.floor((fileListData[i].size)/1024);
+            type.textContent = fileListData[i].type.split("/")[1].toUpperCase();
+            lastModifiedDay.textContent = days[fileListData[i].lastModifiedDate.getDay()];
+            lastModifiedDate.textContent = fileListData[i].lastModifiedDate.toUTCString().split(",")[1].trim().split(" ").slice(0, 3).join(" ");
         }
         demoFnx();
-        
+
         tableData.appendChild(sNo);
         tableData.appendChild(name);
         tableData.appendChild(size);
         tableData.appendChild(type);
-    
-        console.log(tableData);
-        console.log(sNo);
-        console.log(name);
-        console.log(size);
-        console.log(type);
-        
+        tableData.appendChild(lastModifiedDate);
+        tableData.appendChild(lastModifiedDay);
+
+
         table.children[0].appendChild(tableData);
     }
-    
 
 }
