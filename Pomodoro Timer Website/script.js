15567b66fe5ee98b1fc3793f4d007c72036dcab7
Gaurav Chandra Joshi <92198241+Gaurav-Chandra-Joshi@users.noreply.github.com>
2025-09-14 17:36:32 +0530

    Update script.js
    Now, This code has the functionality of updating the pomodoro counts.

--- Pomodoro Timer Website/script.js
+++ Pomodoro Timer Website/script.js
@@ -2,22 +2,40 @@ let timer = document.getElementById("timer");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 let alarm = document.getElementById("audio");
-let time = parseInt(minutes.textContent) * 60;
+// let time = parseInt(minutes.textContent) * 60;
+let time = 1500;
+let totalTime = time;
+
+let pomodoroSessionCount = document.getElementById("pomodoro-session-count");
+
+let dataBase = {
+    pomodoroComplete: 0,
+}
+function updataPomodoroSessionCount(){
+    a = pomodoroSessionCount.textContent.split(" ");
+    a.pop();
+    b = dataBase.pomodoroComplete;
+    c = a.concat(b);
+    d = c.join(" ");
+    pomodoroSessionCount.textContent = d; 
+}
 
 let startBtn = document.getElementById("start");
 startBtn.addEventListener("click", () => {
     alarm.play();
     alarm.pause();
     alarm.currentTime = 0;
-
+    
     startTimer();
 });
 
 function startTimer() {
-    setInterval(() => {
+    let timer = setInterval(() => {
         if (time < 1) {
             alarm.play();
-            clearInterval();
+            dataBase.pomodoroComplete++;
+            updataPomodoroSessionCount();
+            clearInterval(timer);
             return;
         }
         time--;
@@ -29,10 +47,20 @@ function startTimer() {
 
         if (seconds.textContent.length <= 1) seconds.textContent.length == 0 ? seconds.textContent = "00" : seconds.textContent = "0" + seconds.textContent;
 
-    }, 1000)
+    }, 1000);
 }
 
 let pauseResumeBtn = document.getElementById("pause-resume");
 pauseResumeBtn.addEventListener("click", () => {
     alert("this is demo!");
 })
+
+let resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", () => {
+    clearInterval();
+    time = 1500;
+})
+
+/* Here I have to work on the functionality  of remaining time. So that I'll use setInterval again using the remaining time (it will help me in pausing time and when the user will restart the timer).
+=> When the pause button will active then there should be a resume button as well.
+*/
