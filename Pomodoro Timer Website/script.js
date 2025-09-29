6bc59b4eb995c3e83234a2732316b4a725ad6d46
Gaurav Chandra Joshi <92198241+Gaurav-Chandra-Joshi@users.noreply.github.com>
2025-09-29 19:46:36 +0530

    Update script.js

--- Pomodoro Timer Website/script.js
+++ Pomodoro Timer Website/script.js
@@ -5,19 +5,30 @@ let alarm = document.getElementById("audio");
 // let time = parseInt(minutes.textContent) * 60;
 let time = 1500;
 let totalTime = time;
+let pauseBtnCount = 0;
 
-let pomodoroSessionCount = document.getElementById("pomodoro-session-count");
+let psc = document.getElementById("pomodoro-session-count");
+let pauseResumeBtn = document.getElementById("pause-resume");
 
 let dataBase = {
     pomodoroComplete: 0,
 }
-function updataPomodoroSessionCount(){
-    a = pomodoroSessionCount.textContent.split(" ");
-    a.pop();
-    b = dataBase.pomodoroComplete;
-    c = a.concat(b);
-    d = c.join(" ");
-    pomodoroSessionCount.textContent = d; 
+function updatapsc() {
+
+    let pscLocalStorage = parseInt(localStorage.getItem("pomodoro-count"));
+
+    // let finalSessionCount = pscLocalStorage + dataBase.pomodoroComplete;
+    let finalSessionCount = pscLocalStorage + 1;
+    // let finalSessionCount = pscLocalStorage + b;
+    console.log(finalSessionCount)
+
+
+    // psc.textContent = pscLocalStorage == "" || null ? `${d}` : `${finalSessionCount}`;
+    psc.textContent = pscLocalStorage == "" || null ? `${d}` : `Pomodoro Completed: ${finalSessionCount}`;
+    // psc.textContent = pscLocalStorage == "" || "null" ? `${d}` : `${pscLocalStorage + b}`;
+    // psc.textContent = d;
+    // psc.textContent = localStorage.getItem("pomodoro-count") == "" || null ? d : JSON.stringify(JSON.parse(localStorage.getItem("pomodoro-count")) + b);
+    psc.textContent = psc.textContent == null ? 0 : psc.textContent;
 }
 
 let startBtn = document.getElementById("start");
@@ -25,43 +36,99 @@ startBtn.addEventListener("click", () => {
     alarm.play();
     alarm.pause();
     alarm.currentTime = 0;
-    
+
     startTimer();
 });
 
+
+let timeInterval = "";
 function startTimer() {
-    let timer = setInterval(() => {
+    timeInterval = setInterval(() => {
         if (time < 1) {
-            alarm.play();
+            // alarm.play();
+            console.log("Database is the thing which is gonna to happen " + dataBase.pomodoroComplete)
             dataBase.pomodoroComplete++;
-            updataPomodoroSessionCount();
-            clearInterval(timer);
+            // updateLocalStorage(1);
+            updateLocalStorage(dataBase.pomodoroComplete);
+            updatapsc();
+            console.log(`This is database ${dataBase.pomodoroComplete}`);
+
+            clearInterval(timeInterval);
             return;
         }
         time--;
-        minutes.textContent = Math.floor(time / 60);
-        seconds.textContent = time % 60;
-        console.log(time);
-
-        if (minutes.textContent.length <= 1) minutes.textContent.length == 0 ? minutes.textContent = "00" : minutes.textContent = "0" + minutes.textContent;
-
-        if (seconds.textContent.length <= 1) seconds.textContent.length == 0 ? seconds.textContent = "00" : seconds.textContent = "0" + seconds.textContent;
-
+        
+        updateTimer(time);
+        
     }, 1000);
 }
-
-let pauseResumeBtn = document.getElementById("pause-resume");
-pauseResumeBtn.addEventListener("click", () => {
-    alert("this is demo!");
+pauseResumeBtn.addEventListener('click', () => {
+    pauseTimer(time, totalTime);
 })
-
 let resetBtn = document.getElementById("reset");
 resetBtn.addEventListener("click", () => {
-    clearInterval();
-    time = 1500;
-})
+    clearInterval(timeInterval);
+    let confirmation = confirm("Do you really want to reset the timer?");
+    console.log(confirmation)
+    if (confirmation == true) {
+        time = totalTime;
+        updateTimer(time);
+    }
 
-/* Here I have to work on the functionality  of remaining time. So that I'll use setInterval again using the remaining time (it will help me in pausing time and when the user will restart the timer).
-=> When the pause button will active then there should be a resume button as well.
-=> Saving the pomodoro counts in localStorage();
+})
+function pauseTimer(goneTime, totalTime) {
+    let remainingTime = totalTime - goneTime;
+    console.log(remainingTime);
+    clearInterval(timeInterval);
+    pauseBtnCount++;
+    startBtn.textContent = "Resume";
+    startBtn.addEventListener('click', () => {
+        startBtn.textContent = "Start"
+    })
+
+    return remainingTime;
+}
+
+function updateTimer(time) {
+    minutes.textContent = Math.floor(time / 60);
+    seconds.textContent = time % 60;
+    console.log(time);
+    if (minutes.textContent.length <= 1) minutes.textContent.length == 0 ? minutes.textContent = "00" : minutes.textContent = "0" + minutes.textContent;
+
+    if (seconds.textContent.length <= 1) seconds.textContent.length == 0 ? seconds.textContent = "00" : seconds.textContent = "0" + seconds.textContent;
+}
+
+function updateLocalStorage(pomodoroCount) {
+    let a = localStorage.getItem("pomodoro-count");
+    a = (a == "" || null || "null") ? "0" : a;
+    localStorage.setItem("pomodoro-count", JSON.stringify(pomodoroCount));
+    // localStorage.setItem("pomodoro-count", JSON.stringify(JSON.parse(a) + pomodoroCount));
+    // console.log(localStorage.setItem("pomodoro-count", JSON.stringify(parseInt(JSON.parse(localStorage.getItem("pomodoro-count")) + pomodoroCount))));
+
+}
+
+window.onload = () => {
+    let storedPomodoroCount = localStorage.getItem("pomodoro-count") == "" ? 0 : JSON.parse(localStorage.getItem("pomodoro-count"));
+    psc.textContent = ` Pomodoro Completed: ${storedPomodoroCount}`;
+    dataBase.pomodoroComplete = JSON.parse(localStorage.getItem("pomodoro-count"));
+
+    ab();
+}
+
+
+/*
+Now, I have to work on the error which I am getting in the extention of this. That pomodoroCount is not defined in 22:159 and in 43:13.  
+=> I have to work on to increase the alarm ture list with more sound effects.
+*/
+
+function ab() {
+    time = 3;
+    startTimer();
+}
+
+/*
+This code is good and now I have to work on the bug that when the timer is reaching zero then it is adding two in the pomodoro count (which is wrong)
+and adding 1 in local Storage (which is right)
+
+I have to work on the code that there is somewhere the increment is happening 2 time which is wrong.
 */
