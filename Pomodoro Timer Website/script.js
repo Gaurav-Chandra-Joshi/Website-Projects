37d12eb840c0b6dd8f56d93cc35899340ac8e7ed
Gaurav Chandra Joshi <92198241+Gaurav-Chandra-Joshi@users.noreply.github.com>
2025-10-01 19:27:16 +0530

    Update script.js
    Now it is capable to update everything properly. Now, I am working on the mode-buttons functionality.

--- Pomodoro Timer Website/script.js
+++ Pomodoro Timer Website/script.js
@@ -2,8 +2,7 @@ let timer = document.getElementById("timer");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 let alarm = document.getElementById("audio");
-// let time = parseInt(minutes.textContent) * 60;
-let time = 1500;
+let time = 25 * 60;
 let totalTime = time;
 let pauseBtnCount = 0;
 
@@ -17,17 +16,11 @@ function updatapsc() {
 
     let pscLocalStorage = parseInt(localStorage.getItem("pomodoro-count"));
 
-    // let finalSessionCount = pscLocalStorage + dataBase.pomodoroComplete;
-    let finalSessionCount = pscLocalStorage + 1;
-    // let finalSessionCount = pscLocalStorage + b;
+    let finalSessionCount = pscLocalStorage;
     console.log(finalSessionCount)
 
 
-    // psc.textContent = pscLocalStorage == "" || null ? `${d}` : `${finalSessionCount}`;
     psc.textContent = pscLocalStorage == "" || null ? `${d}` : `Pomodoro Completed: ${finalSessionCount}`;
-    // psc.textContent = pscLocalStorage == "" || "null" ? `${d}` : `${pscLocalStorage + b}`;
-    // psc.textContent = d;
-    // psc.textContent = localStorage.getItem("pomodoro-count") == "" || null ? d : JSON.stringify(JSON.parse(localStorage.getItem("pomodoro-count")) + b);
     psc.textContent = psc.textContent == null ? 0 : psc.textContent;
 }
 
@@ -45,7 +38,7 @@ let timeInterval = "";
 function startTimer() {
     timeInterval = setInterval(() => {
         if (time < 1) {
-            // alarm.play();
+            alarm.play();
             console.log("Database is the thing which is gonna to happen " + dataBase.pomodoroComplete)
             dataBase.pomodoroComplete++;
             // updateLocalStorage(1);
@@ -102,33 +95,10 @@ function updateLocalStorage(pomodoroCount) {
     let a = localStorage.getItem("pomodoro-count");
     a = (a == "" || null || "null") ? "0" : a;
     localStorage.setItem("pomodoro-count", JSON.stringify(pomodoroCount));
-    // localStorage.setItem("pomodoro-count", JSON.stringify(JSON.parse(a) + pomodoroCount));
-    // console.log(localStorage.setItem("pomodoro-count", JSON.stringify(parseInt(JSON.parse(localStorage.getItem("pomodoro-count")) + pomodoroCount))));
-
 }
 
 window.onload = () => {
     let storedPomodoroCount = localStorage.getItem("pomodoro-count") == "" ? 0 : JSON.parse(localStorage.getItem("pomodoro-count"));
     psc.textContent = ` Pomodoro Completed: ${storedPomodoroCount}`;
     dataBase.pomodoroComplete = JSON.parse(localStorage.getItem("pomodoro-count"));
-
-    ab();
 }
-
-
-/*
-Now, I have to work on the error which I am getting in the extention of this. That pomodoroCount is not defined in 22:159 and in 43:13.  
-=> I have to work on to increase the alarm ture list with more sound effects.
-*/
-
-function ab() {
-    time = 3;
-    startTimer();
-}
-
-/*
-This code is good and now I have to work on the bug that when the timer is reaching zero then it is adding two in the pomodoro count (which is wrong)
-and adding 1 in local Storage (which is right)
-
-I have to work on the code that there is somewhere the increment is happening 2 time which is wrong.
-*/
