79bb03962dcc7eeca9086f6d13df7c9c0cd4e8d1
Gaurav Chandra Joshi <92198241+Gaurav-Chandra-Joshi@users.noreply.github.com>
2025-10-11 19:42:01 +0530

    Update script.js
    This code is so much advance.

--- Pomodoro Timer Website/script.js
+++ Pomodoro Timer Website/script.js
@@ -14,7 +14,7 @@ let circle = document.querySelector(".circle");
 let alarm = document.getElementById("audio");
 
 let currentMode;
-const duration = {
+let durations = {
     work: (25 * 60),
     short: (5 * 60),
     long: (15 * 60)
@@ -54,19 +54,20 @@ startBtn.addEventListener("click", () => {
 
     switch (activeMode) {
         case "work":
-            startTimer(duration.work);
+            startTimer(durations.work * 60);
             break;
         case "short":
-            startTimer(duration.short);
+            startTimer(durations.short * 60);
             break;
         case "long":
-            startTimer(duration.long);
+            startTimer(durations.long * 60);
             break;
     }
     isRunning = true;
 })
 
 function startTimer(time) {
+    if (!timer === null) return;
     if (isRunning) return;
 
     timer = setInterval(() => {
@@ -74,11 +75,13 @@ function startTimer(time) {
         if (isRunning && (currentMode !== nextMode)) {
             resetTimer();
             clearInterval(timer);
+            timer = null;
             return;
         }
 
         if (isPaused) {
             clearInterval(timer);
+            timer = null;
             remainingTime = time;
             console.log("The remaining time is :- ", remainingTime);
             return;
@@ -87,12 +90,13 @@ function startTimer(time) {
             alarm.play();
             setTimeout(() => updateDisplay(currentMode), 2000)
             clearInterval(timer);
+            timer = null;
             isRunning = false;
             alert("Your time has reached at 0!");
             return;
         }
 
-        console.log(time);
+        // console.log(time);
 
         remainingTime = time--;
 
@@ -109,17 +113,17 @@ pauseBtn.addEventListener("click", () => {
 })
 
 function pauseTimer(remainingTime) {
+    if (!isRunning && !isPaused) return;
     if (isPaused) {
         pauseBtn.textContent = "⏸";
-        startTimer(remainingTime);
+        if (timer === null) startTimer(remainingTime);
         isPaused = false;
         isRunning = true;
-        console.log('This is the condition of isPaused is true!')
     } else {
         pauseBtn.textContent = "⏯";
         console.log(remainingTime);
-        isRunning = false;
         isPaused = true;
+        isRunning = false;
     }
 }
 
@@ -128,38 +132,97 @@ let resetBtn = document.getElementById("reset");
 resetBtn.addEventListener("click", () => resetTimer())
 function resetTimer() {
     clearInterval(timer);
+    timer = null;
     isReseted = true;
+
+    if (isPaused) {
+        isPaused = false;
+        pauseBtn.textContent = "⏸";
+    }
     isRunning = false;
 
     updateDisplay(currentMode);
 }
 
+window.onload = () => {
+    switchMode();
+
+    let saved = JSON.parse(localStorage.getItem("Durations") || "null");
+    if (saved && typeof saved == "object") {
+        durations = saved;
+    } else {
+        updateLocalStorage(durations);
+    }
+
+    console.log(durations);
+    updateSettingBoxes();
+    updateDisplay("Work");
+}
+
+// Setting Button functionality
+let settingBtn = document.querySelector(".modal");
+let workDuration = document.querySelector("#work-duration");
+let shortDuration = document.querySelector("#short-duration");
+let longDuration = document.querySelector("#long-duration");
+
 function updateDisplay(currentMode) {
     switch (currentMode) {
         case "Work":
-            minutes.textContent = "25";
+            minutes.textContent = workDuration.value;
             seconds.textContent = "00";
             circle.style.borderColor = "#FF5A5F";
             break;
         case "Short Break":
-            minutes.textContent = "5";
+            minutes.textContent = shortDuration.value;
             seconds.textContent = "00";
             circle.style.borderColor = "#00C896";
             break;
         case "Long Break":
-            minutes.textContent = "15";
+            minutes.textContent = longDuration.value;
             seconds.textContent = "00";
             circle.style.borderColor = "#6C63FF";
             break;
     }
 }
 
-window.onload = () => {
-    switchMode();
+function settingOn() {
+    settingBtn.style.display = "flex";
+}
+
+function settingOff() {
+    settingBtn.style.display = "none";
+    updateSettingBoxes();
+}
+
+function saveSetting() {
+    alert("Setting Saved!")
+    updateDisplay(currentMode);
+    updateLocalStorage(durations);
+    updateDurationObject();
+    durations = JSON.parse(localStorage.getItem("Durations"));
+    settingOff();
+}
+
+function updateSettingBoxes() {
+    workDuration.value = durations.work;
+    shortDuration.value = durations.short;
+    longDuration.value = durations.long;
+}
+
+function updateLocalStorage(durationObject) {
+    localStorage.setItem("Durations", JSON.stringify(durationObject));
+}
+
+let settingInputBoxDurations;
+function updateDurationObject() {
+    durations.work = Number(workDuration.value);
+    durations.short = Number(shortDuration.value);
+    durations.long = Number(longDuration.value);
+
+    console.log(durations);
+    updateLocalStorage(durations);
 }
 
 /*
     1. I have to work on the circular timer ring and I have to make that ring function.
-
-    2. I have to work upon the setting feature.
 */
